6bdfc4d315eb8ebc0e681a0dfd2b5e1627ff31cd
Conrado Pinheiro <conradopsa@gmail.com>
2020-08-18 04:17:34 -0300

    Refatoração responseSequelizeError

--- src/service/usuariosService.ts
+++ src/service/usuariosService.ts
@@ -1,21 +1,18 @@
 import { Request, Response } from 'express';
 import { Error } from 'sequelize';
-import { red } from 'chalk';
-import { ResponseError, generateResponseError } from '../utils/serviceResponse';
+import { responseSequelizeError } from '../utils/serviceResponse';
 import Usuario from '../models/usuario';
 
 class UsuariosService {
 
     async listUsuarios(request: Request, response: Response) {
 
-        await Usuario.findAll().then((users) => {
-            response.json(users);
-        }).catch((err: Error) => {
-            const responseError: ResponseError = generateResponseError(err);
-            response.status(500).json(responseError);
-
-            console.error(red(err.stack));
-        });
+        await Usuario.findAll().then(
+            (users) => {
+                response.json(users);
+            })
+            .catch((err: Error) =>
+                responseSequelizeError(err, response));
 
     }
 }
--- src/utils/serviceResponse.ts
+++ src/utils/serviceResponse.ts
@@ -1,10 +1,20 @@
-export function generateResponseError(error: Error): ResponseError {
+import { Response } from 'express';
+import { red } from 'chalk';
+
+function generateSequelizeResponseError(error: Error): ResponseError {
     return {
         error: {
             message: error.message,
             stack: error.stack
         }
     }
+}
+
+export function responseSequelizeError(error: Error, response: Response) {
+    const responseError: ResponseError = generateSequelizeResponseError(error);
+    response.status(500).json(responseError);
+
+    console.error(red(error.stack));
 }
 
 export interface ErrorMessage {
